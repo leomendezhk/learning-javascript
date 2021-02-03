7e3c0b2c0d13dc4f9bf18e2a603a9b5563a0ecec
Leo Mendez <leomendezhk@gmail.com>
2021-02-03 23:16:49 +0800

    Updated Lecture on Functions

--- fundamentals-part-2/assignments.js
+++ fundamentals-part-2/assignments.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /////////////////////////////////////////////////////////////////////
-
+/*
 // LECTURE: FUNCTIONS
 //1
 function describeCountry(country, population, capitalCity) {
@@ -15,8 +15,51 @@ const portugalData = describeCountry('Portugal', 33, 'Lisbon');
 console.log(portugalData);
 const findlandData = describeCountry('Findland', 6, 'Helsinski');
 console.log(findlandData);
+*/
 
 /////////////////////////////////////////////////////////////////////
+
+/*
+//LECTURE: Function Declarations vs. Expressions
+//1,2
+const worldPopulation = 7900;
+function percentageOfWorld1(population) {
+  return (population / worldPopulation) * 100;
+}
+//3
+const chinaPopulation = percentageOfWorld1(1441);
+const argentinaPopulation = percentageOfWorld1(40);
+const portugalPopulation = percentageOfWorld1(33);
+console.log(chinaPopulation, argentinaPopulation, portugalPopulation);
+
+//4
+const percentageOfWorld2 = function(population) {
+  return (population / worldPopulation) * 100;
+}
+const chinaPopulation2 = percentageOfWorld2(1441);
+console.log(chinaPopulation2);
+*/
+
+/////////////////////////////////////////////////////////////////////
+
+/*
+//LECTURE: Arrow Functions
+//1
+const percentageOfWorld3 = population => (population / 7900) * 100;
+
+const chinaPopulation3 = percentageOfWorld3(1441);
+console.log(chinaPopulation3); 
+*/
+
+/////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
 
  
 
--- fundamentals-part-2/script.js
+++ fundamentals-part-2/script.js
@@ -4,6 +4,7 @@
 
 /////////////////////////////////////////////////////////////////////
 
+/*
 // LECTURE: FUNCTION
 // They allow us to call line of code many times during our program
 
@@ -29,6 +30,61 @@ console.log(appleJuice);
 
 const applesOrangesJuice = fruitProcessor (2, 3);
 console.log(applesOrangesJuice); 
+*/
 
 /////////////////////////////////////////////////////////////////////
+/*
+//LECTURE: FUNCTION DECLARATION AND EXPRESSION
+//Funcion store values and like any value we can store it on a variable.
+//What is the main difference between declaration and expression?
+//Its that function declaration can be called before write the function
+
+//Function Declaration
+const age1 = calcAge1(1994); //called before
+
+function calcAge1(birthYear) {
+  return 2021 - birthYear;
+}
+
+console.log(age1);
+
+//Function Expression
+// create a variable to store the fuction
+const calcAge2 = function (birthYear) {
+  return 2021 - birthYear;
+}
+
+const age2 = calcAge2(1994); //called after
+console.log(age2);
+
+//Which type of fuction is better to use? 
+//Depens of me, function expression obligate me to write the fuction before colled it and also can be stored in variable, so It may be more organized use function expression.
+*/
+
+/////////////////////////////////////////////////////////////////////
+
+/*
+//LECTURE: ARROW FUNCTION
+//Simples way to write a function 
+//idealy when fuction is just one line of code
+
+const calcAge3 = birthYear => 2021 - birthYear;
+const age3 = calcAge3(1994);
+console.log(age3);
+
+//Essenario when there is more than one line of code
+const yearUntilRetirement = (birthYear, firstName) => {
+  const age = 2021 - birthYear; //first calc current age
+  const retirement = 65 - age; // then calc retirement
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearUntilRetirement(1994, 'Leo'));
+console.log(yearUntilRetirement(1998, 'Alan'));
+*/
+
+/////////////////////////////////////////////////////////////////////
+
+
 
