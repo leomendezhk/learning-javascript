06fc55b76543ef74cd7bd4d801631bfdfb6a7439
Leo Mendez <leomendezhk@gmail.com>
2021-04-28 12:04:12 +0800

    Destructuring Arrays

--- 09- Data Structures/script.js
+++ 09- Data Structures/script.js
@@ -7,284 +7,73 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-
+  //Creater a method that return an array with the value at position given (index).
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
-
-  //function with object as argument
-  orderDelivery: function ({
-    starterIndex = 1,
-    mainIndex = 1,
-    time = '20:00',
-    address,
-  }) {
-    console.log(
-      `Order received: ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be deliver to ${address} at ${time} `
-    );
-  },
-
-  //Order Pasta function
-  orderPasta: function (ing1, ing2, ing3) {
-    console.log(`Your pasta with ${ing1}, ${ing2}, ${ing3}`);
-  },
-
-  orderPizza: function (mainIngredient, ...otherIngredients) {
-    console.log(mainIngredient);
-    console.log(otherIngredients);
-  },
 };
 
-//LECTURE: NULLISH ??
-//consider only null or undefines (NOT '' or 0)
-//only if value is null or undefined it continues
-//in the example the evaluation is 0, so return 0. if it were undefined it will return 10
-
-restaurant.numGuests = 0;
-const guest2 = restaurant.numGuests ?? 10;
-console.log(guest2);
-
-/////////////////////////////////////////////////////////////////////
 /*
+//LECTURE: DESTRUCTURING ARRAYS
 
-// LECTURE: REST PATTERN & REST PARAMETERS
+//Destructuring is to break a complex data structure from an array or an object into separate variables.
+//For arrays we use destructurig to retrieve elements from the array and store it into smaller variables.
+//Even when we destructure, the original array is not afected.
 
-//rest goes on the left of = , and spread on the right.
-//rest is always the last value and doesnt count skipped elements
+//RETRIEVE DATA INTO VARIABLES.
 
-const [pizza, , rissoto, ...otherFood] = [
-  ...restaurant.mainMenu,
-  ...restaurant.starterMenu,
-];
-console.log(pizza, rissoto, otherFood);
-
-//Objects
-const { sat, ...weekdays } = restaurant.openingHours;
-console.log(weekdays);
-
-//Functions REST
-//take multiple values and packet into arrays
-
-const add = function (...numbers) {
-  let sum = 0;
-  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
-  console.log(sum);
-};
-
-add(1, 2, 3);
-add(4, 1, 2, 5, 6, 7);
-add(1, 2, 2, 3, 5, 7, 8, 9);
-
-const x = [2, 3, 4];
-add(...x);
-
-restaurant.orderPizza('mushroom', 'tomato', 'pineapple', 'spinach');
-restaurant.orderPizza('mushroom'); //empty array for other argument
-*/
-
-/////////////////////////////////////////////////////////////////////
-
-/*
-//LECTURE: SHORT-CIRCUITING
-
-//or ||
-
-//Use ANY data type, return ANY data type, short-circuiting
-//returm first true value 
-console.log(3 || 'Jonas');
-console.log('' || 'Jonas');
-console.log(undefined || null);
-
-// restaurant.numGuests = 24;
-const guest1 = restaurant.numGuests ? restaurant.numGuests : 10;
-console.log(guest1);
-//or use short-circuiting instead
-const guest2 = restaurant.numGuests || 23;
-console.log(guest2);
-
-//AND &&
-//return the falsy value or the last true value if all values are true
-
-console.log(3 && 'Leo');
-console.log(null && 2);
-console.log(23 && 'hello' && '' && undefined);
-
-//Practical example
-if (restaurant.orderPizza) {
-  restaurant.orderPizza('mushroom', 'spinach');
-}
-//easier way to check if exist and order
-restaurant.orderPizza && restaurant.orderPizza('mushroom', 'spinach');
-
-*/
-//////////////////////////////////////////////////////////////////////////
-/*
-// LECTURE: THE SPREAD OPERATOR ...
-const arr = [7, 8, 9];
-const arrBad = [1, 2, arr[0], arr[1], arr[2]];
-console.log(arrBad);
-
-const arrGood = [1, 2, ...arr];
-console.log(...arrGood); //individuals
-console.log(arrGood); //in array
-
-// create new array with added more options
-const newMenu = [...restaurant.mainMenu, 'Gnocci'];
-console.log(newMenu);
-
-//Copy array
-const mainMenuCopy = [...restaurant.mainMenu];
-console.log(mainMenuCopy);
-
-//Join two array
-const twoMenu = [...mainMenuCopy, ...newMenu];
-console.log(twoMenu);
-
-//Iterables: array, string, maps, sets. NOT objects.
-const str = 'Leo';
-const letters = [...str, , '.St'];
-console.log(letters);
-console.log(...str);
-
-//Real world example order pasta
-// const ingredients = [
-//   prompt("Let's make pasta! ingredient 1?"),
-//   prompt('ingredient 2?'),
-//   prompt('ingredient 3?'),
-// ];
-
-// restaurant.orderPasta(...ingredients);
-
-//Objects
-const newRestaurant = {
-  founded: 1994,
-  ...restaurant,
-  founder: 'Carlo Ppepotti',
-};
-console.log(newRestaurant);
-newRestaurant.name = 'Las Cholas';
-console.log(newRestaurant.name);
-console.log(restaurant.name);
-*/
-
-///////////////////////////////////////////////////////////
-/*
-//calling function objects
-restaurant.orderDelivery({
-  time: '23:30',
-  address: 'Via del Sole 351',
-  mainIndex: 2,
-  starterIndex: 2,
-});
-
-//calling Using default values
-restaurant.orderDelivery({
-  address: 'Via del Sole 351',
-  starterIndex: 1,
-});
-
-// LECTURE: DESTRUCTURING OBJECTS
-//Specific name, doest matter order and use curly brace
-
-const { name, openingHours, categories } = restaurant;
-console.log(name, openingHours, categories);
-
-//changing name variables
-const {
-  name: restaurantName,
-  openingHours: hours,
-  categories: tags,
-} = restaurant;
-console.log(restaurantName, hours, tags);
-
-//Setting defaul values
-const { menu = [], starterMenu = (starters = []) } = restaurant;
-console.log(menu, starterMenu);
-
-//Mutating variables
-let a = 111;
-let b = 999;
-const obj = { a: 23, b: 7, c: 1 };
-({ a, b } = obj); //use parentesis to avoid error
-console.log(a, b);
-
-//Nested Objects
-
-const {
-  fri: { open, close },
-} = openingHours;
-console.log(open, close);
-
-*/
-///////////////////////////////////////////////////////////
-/*
-// LECTURE: Destructuring Arrays
-//breakdown complex data into small variables.
-
+//without destructuring.
 const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
 const c = arr[2];
 
-//destructuring syntax
+//destructuring syntax.
 const [x, y, z] = arr;
-console.log(x, y, z);
-console.log(arr);
+console.log(x, y, z); // 2 3 4.
 
-//Destructuring restaurant categories
-//living a space in the middle to skip order
-
+//Retrieve from 'restaurant'.
+//leaving a space on the syntax skip the value of the element order.
 let [main, , secondary] = restaurant.categories;
-console.log(main, secondary);
+console.log(main, secondary); // Italian Vegetarian.
+
+///////////////////////////////////////////////////////////////////
 
 //SWITCHING VARIABLES
-//old way
-// const temp = main;
-// main = secondary;
-// secondary = temp;
-// console.log(main, secondary);
 
-//using destructuring
+//without destructuring.
+const temp = main;
+main = secondary;
+secondary = temp;
+console.log(main, secondary); // secondary main
+
+//with destructuring
 [main, secondary] = [secondary, main];
-console.log(main, secondary);
 
-///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
 
-//Receive 2 retun values from a function
-const [starter, mainCourse] = restaurant.order(2, 0);
-console.log(starter, mainCourse);
+//Creating two variables out of the output of a function (order)
 
-////////////////////////////////////////////////////////
+const [starter, mainCourse] = restaurant.order(0, 2);
+console.log(starter, mainCourse); //Focaccia Risotto.
 
-// Destructuring from NESTED Array
+//////////////////////////////////////////////////////////////////
+
+//NESTED  DESTRUCTURING ARRAY
 
 const nested = [2, 4, [5, 6]];
 // const [i, , j] = nested;
-// console.log(i, j);
-
+// console.log(i, j); // 2 [5,6]
 const [i, , [j, k]] = nested;
-console.log(i, j, k);
+console.log(i, j, k); // 2 5 6
 
-///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
 
-//Default values
-//Asigning value to an array before know the actual value
+//DEFAULT VALUES.
+//add a default value to a variable, so in case there is no value it can use the default.
+//if variable doesnt have default value it will show 'undefined'.
+
 const [p = 1, q = 1, r = 1] = [8, 9];
-console.log(p, q, r); // 8 9 1
+console.log(p, q, r); //8 9 1 
 
-///////////////////////////////////////////////////////////
 */
