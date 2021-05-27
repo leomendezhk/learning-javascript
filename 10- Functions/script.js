31d0e077ae55b6abfb60bd330b2c19146d50d65b
leomendezhk <leomendezhk@gmail.com>
2021-05-27 19:48:38 +0800

    The Call and Apply methods

--- 10- Functions/script.js
+++ 10- Functions/script.js
@@ -114,3 +114,82 @@ document.body.addEventListener('click', highFive);
 
 ////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////
+
+//LECTURE 5: FUNCTION RETUR OTHER FUNCTION
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greetHey = greet('Hey');
+greetHey('Leo');
+
+greet('Hello')('Leo');
+
+//Make it arrow function
+
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+
+greetArr('Hi')('Leo');
+
+////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+// LECTURE 6: The call and apply methods
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iatacode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} fligh ${this.iatacode}${flightNum}`
+    );
+
+    this.bookings.push({ flight: `${this.iatacode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Leo Mendez');
+lufthansa.book(635, 'Jonas Smith');
+
+const eurowings = {
+  name: 'Eurowings',
+  iatacode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+// book(23, 'Sarah Williams') . Does not work
+
+//now this var is a function, because it copy from the method. But it's not a method related to the object lufthansa anymore. It's an independet function now, so the this keyword  inside the function doesnt relate. Will give undefined.
+//So we have to manually point to were 'this' is related too (if lufthansa or eurowings) to make it work.
+
+//Call method. pointer, flishtNum, name.
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 234, 'Mary Cooper');
+console.log(lufthansa);
+
+///////////////////////////////////////////////////////////
+
+//Created another airline with same key names if not does not work
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iatacode: 'SW',
+  bookings: [],
+};
+
+//Apply method, similar to call method but need pointer, array of data
+
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData); //another option
+
+//////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
